986586f06717d00bfc5e7ae754b367a58127e915
vondracek <luvo@ciklum.com>
2025-03-12 09:30:55 +0100

    testing runCatching

--- src/main/kotlin/cz/sg/kotlinexamples/TestingMain.kt
+++ src/main/kotlin/cz/sg/kotlinexamples/TestingMain.kt
@@ -1,26 +1,41 @@
 package cz.sg.kotlinexamples
 
-import java.io.FileInputStream
-import java.util.zip.ZipInputStream
 
-fun main(args: Array<String>) {
-    tryToUnzipFile("dmp_building_lowell_202410.zip")
-    tryToUnzipFile("dmp_building_lowell_202409.zip")
+fun main() {
+
+    try {
+        method1()
+    } catch (e: Exception) {
+        println("error1")
+    }
+
+    try {
+        method2()
+    } catch (e: Exception) {
+        println("error2")
+    }
+
+    // -----
+
+    try {
+        method1WithProblem()
+        try {
+            method2()
+        } catch (e: Exception) {
+            println("error2")
+        }
+    } catch (e: Exception) {
+        println("error1")
+    }
 }
 
-fun tryToUnzipFile(fileName: String) {
-    try {
-        val zipStream = ZipInputStream(FileInputStream("/Users/vondracek/tmp/ANS2/zip/$fileName"))
-        val entry = zipStream.nextEntry
-        if (entry != null) {
-            println("File $fileName was unzipped correctly")
-        } else {
-            zipStream.closeEntry()
-            zipStream.close()
-            println("Unable to unzip file $fileName")
-        }
-
-    } catch (e: Exception) {
-        println("exception")
-    }
+private fun method1() = println("method1")
+private fun method1WithProblem() {
+    println("method1")
+    throw Exception("exception1")
+}
+private fun method2() = println("method2")
+private fun method2WithProblem() {
+    println("method2")
+    throw Exception("exception2")
 }
